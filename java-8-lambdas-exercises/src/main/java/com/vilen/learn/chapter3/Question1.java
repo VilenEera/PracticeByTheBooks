9b63d9e930b92b6dccdd4c8e14c23210fdb017f5
VilenEera <chhx4567@163.com>
2017-10-07 17:42:36 +0800

    Chapter3 练习1 finish

--- java-8-lambdas-exercises/src/main/java/com/vilen/learn/chapter3/Question1.java
+++ java-8-lambdas-exercises/src/main/java/com/vilen/learn/chapter3/Question1.java
@@ -1,5 +1,11 @@
 package com.vilen.learn.chapter3;
 
+import com.vilen.learn.chapter1.Album;
+import com.vilen.learn.chapter1.Artist;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -9,5 +15,15 @@ public class Question1 {
     // 求和函数
     public static int addUp(Stream<Integer> numbers) {
         return numbers.reduce(0, (acc, x) -> acc + x);
+    }
+    // 编写一个函数，接受艺术家列表作为参数，返回一个字符串列表，其中包含艺术家的姓名和国籍；
+    public static List<String> getNamesAndOrigins(List<Artist> artists){
+        return artists.stream().flatMap(artist -> Stream.of(artist.getName(),artist.getNationality()))
+                .collect(Collectors.toList());
+    }
+
+    // 接受专辑列表作为参数，返回一个由最多包含3首歌曲的专辑组成的列表
+    public static List<Album> getAlbumsWithAtMostThreeTracks(List<Album> input) {
+        return input.stream().filter(album -> album.getTrackList().size() <= 3).collect(Collectors.toList());
     }
 }
--- java-8-lambdas-exercises/src/test/java/com/vilen/learn/chapter3/Question1Test.java
+++ java-8-lambdas-exercises/src/test/java/com/vilen/learn/chapter3/Question1Test.java
@@ -1,9 +1,13 @@
 package com.vilen.learn.chapter3;
 
+import com.vilen.learn.chapter1.Album;
+import com.vilen.learn.chapter1.SampleData;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
 import static org.junit.Assert.*;
 
 /**
@@ -16,5 +20,24 @@ public class Question1Test {
         assertEquals(0,result);
         int result2 = Question1.addUp(Stream.of(1, 2, 3));
         assertEquals(6,result2);
+    }
+
+    @Test
+    public void testAddsListWithValues() throws Exception {
+        int result = Question1.addUp(Stream.of(1, 3, -2));
+        assertEquals(2,result);
+    }
+
+    @Test
+    public void testExtractsNamesAndOriginsOfArtists() throws Exception {
+        List<String> namesAndOrigins = Question1.getNamesAndOrigins(SampleData.getThreeArtists());
+        assertEquals(asList("John Coltrane","US","John Lennon","UK","The Beatles","UK"),namesAndOrigins);
+    }
+
+    @Test
+    public void testFindsAlbums() throws Exception {
+        List<Album> input = asList(SampleData.manyTrackAlbum, SampleData.sampleShortAlbum, SampleData.aLoveSupreme);
+        List<Album> result = Question1.getAlbumsWithAtMostThreeTracks(input);
+        assertEquals(asList(SampleData.sampleShortAlbum,SampleData.aLoveSupreme),result);
     }
 }
