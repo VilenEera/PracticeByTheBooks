465b0ac26e727a16bf31d5a8f87c1542294e6f72
VilenEera <chhx4567@163.com>
2017-10-09 20:29:09 +0800

    Chapter4 Parent Overriding

---
+++ java-8-lambdas-exercises/src/main/java/com/vilen/learn/chapter4/part6/OverridingChild.java
@@ -0,0 +1,7 @@
+package com.vilen.learn.chapter4.part6;
+
+/**
+ * Created by vilen on 2017/10/9.
+ */
+public class OverridingChild extends OverridingParent implements Child {
+}
--- java-8-lambdas-exercises/src/test/java/com/vilen/learn/chapter4/part6/ParentTest.java
+++ java-8-lambdas-exercises/src/test/java/com/vilen/learn/chapter4/part6/ParentTest.java
@@ -30,5 +30,16 @@ public class ParentTest {
         assertEquals("Class Parent: Hi!", parent.getLastMessage());
     }
 
+    // 最后，调用的是OverridingParent 中的welcome 方法，而不是Child 接口中定义的默认方法
+    // 原因在于，与接口中定义的默认方法相比，类中重写的方法更具体
+
+    // 简言之，类中重写的方法胜出 。这样的设计主要是由增加默认方法的目的决定的，增加默认方法主要是为了在接口上向后兼容。
+    // 让类中重写方法的优先级高于默认方法能简化很多继承问题
+    @Test
+    public void concreteBeatsCloserDefault() {
+        Child child = new OverridingChild();
+        child.welcome();
+        assertEquals("Class Parent: Hi!", child.getLastMessage());
+    }
 
 }
