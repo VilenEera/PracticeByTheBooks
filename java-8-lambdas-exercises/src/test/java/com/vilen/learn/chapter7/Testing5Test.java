f6ff7963cc9e1935bdc27deb2ac129eb1d81c967
chhx4567 <chhx4567@163.com>
2017-10-15 11:15:43 +0800

    Chapter7 测试替身

--- java-8-lambdas-exercises/src/test/java/com/vilen/learn/chapter7/Testing5Test.java
+++ java-8-lambdas-exercises/src/test/java/com/vilen/learn/chapter7/Testing5Test.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by vilen on 2017/10/15.
@@ -35,6 +37,16 @@ public class Testing5Test {
         assertEquals("Ab", result);
     }
 
-
-
+    //使用Mockito 这样的框架有助于更容易地产生测试替身。
+    private List<Integer> otherList = Arrays.asList(1, 2, 3);
+    @Test
+    public void mockitoLambdas() {
+        //我们不想返回List 本上的长度，而是返回另一个List 的长度，
+        //为了模拟List 的size 方法，我们不想只给出答案，还想做一些操作，因此传入一个Lambda表达式
+        List<String> list = mock(List.class);
+        when(list.size()).thenAnswer(invocationOnMock -> otherList.size());
+        assertEquals(3, list.size());
+    }
+    //Mockito使用Answer 接口允许用户提供其他行为，换句话说，这是我们的老朋友：代码即数据。
+    //之所以在这里能使用Lambda表达式，是因为Answer 本身就是一个函数接口
 }
